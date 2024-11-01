f4cde89a179388ee6b93d87518062483184f9e02
haru-0205 <rm23081b@st.omu.ac.jp>
2024-11-01 23:51:41 +0900

    update ui

---
+++ lua/plugins/112_incline.lua
@@ -0,0 +1,10 @@
+local devicons = require 'nvim-web-devicons'
+
+return {
+  "b0o/incline.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "lewis6991/gitsigns.nvim",
+  },
+  opts = {}
+}
---
+++ lua/plugins/113_winsep.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-zh/colorful-winsep.nvim",
+  opts = {
+    hi = {
+      bg = "",
+      fg = "#7ba9e9"
+    }
+  }
+}
